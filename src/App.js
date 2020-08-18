7e6e0238ec7f77dc5221205ad099a2f6fddc508d
Ahmet Feyzi Halaç <ahmetfeyzihalac@gmail.com>
2020-08-18 10:26:04 +0300

    Created project with CRA

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>JotForm New Tab</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
