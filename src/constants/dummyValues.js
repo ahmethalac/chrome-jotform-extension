193e8fe31bf43cb53f5f03c5e9f747203400dffd
Ahmet Feyzi Halaç <ahmetfeyzihalac@gmail.com>
2020-08-25 16:27:09 +0300

    Data structure changed to map

--- src/constants/dummyValues.js
+++ src/constants/dummyValues.js
@@ -40,24 +40,44 @@
  * }
  */
 
-export const DUMMY_STATE_FOR_TODOLISTS = [
-  {
-    name: 'List1',
+export const DUMMY_STATE_FOR_TODOLISTS = {
+  1: {
     id: 1,
-    todos: [
-      { id: 5, name: 'list1todo1', done: false },
-      { id: 3, name: 'list1todo2', done: true },
-    ],
+    name: 'List1',
+    todos: {
+      1: {
+        id: 1,
+        name: 'list1todo1',
+        done: false,
+      },
+      2: {
+        id: 2,
+        name: 'list1todo2',
+        done: true,
+      },
+    },
   },
-  {
-    name: 'List2',
+  2: {
     id: 2,
-    todos: [
-      { id: 2, name: 'list2todo1', done: false },
-      { id: 4, name: 'list2todo2', done: false },
-      { id: 7, name: 'list2todo3', done: true },
-    ],
+    name: 'List2',
+    todos: {
+      3: {
+        id: 3,
+        name: 'list2todo1',
+        done: false,
+      },
+      4: {
+        id: 4,
+        name: 'list2todo2',
+        done: false,
+      },
+      5: {
+        id: 5,
+        name: 'list2todo3',
+        done: false,
+      },
+    },
   },
-];
+};
 
 export default 'dummyExportForESLINT';
--- src/selectors/todosSelectors.js
+++ src/selectors/todosSelectors.js
@@ -1,6 +1,6 @@
 export const getName = object => object.get('name', 'undefined');
 export const getID = object => object.get('id', 0);
 
-export const getTodoLists = state => state.todoLists;
-export const getTodos = todoList => todoList.get('todos', []);
+export const getTodoLists = state => state.todoLists.toArray().map(value => value[1]);
+export const getTodos = todoList => todoList.get('todos', []).toArray().map(value => value[1]);
 export const getDone = todo => todo.get('done', false);
