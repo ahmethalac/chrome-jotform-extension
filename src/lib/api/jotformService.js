da2ff0881e68ac928bef037b63c292470470534c
Ahmet Feyzi Halaç <ahmetfeyzihalac@gmail.com>
2020-08-27 13:02:24 +0300

    jotformService refactored (Checkbox for status)

--- src/lib/api/jotformService.js
+++ src/lib/api/jotformService.js
@@ -7,9 +7,12 @@ export const createTodoList = name => {
   const requestBody = {
     questions: [
       { type: 'control_textbox', text: 'Todo Name', order: '1' },
-      { type: 'control_textbox', text: 'Done', order: '2' },
+      {
+        type: 'control_radio', text: 'Status', order: '2', options: 'Done|Not Done',
+      },
+      { type: 'control_button', text: 'Submit', order: '3' },
     ],
-    properties: { title: `todoList_${name}` },
+    properties: { title: `todoList_${name}`, type: 'CARD' },
   };
   return axios.put(`${baseURL}user/forms?apiKey=${JOTFORM_API_KEY}`, requestBody);
 };
@@ -18,14 +21,14 @@ export const submitTodo = (formId, name) => {
   const requestBody = [
     {
       1: { text: name },
-      2: { text: 'false' },
+      2: { text: 'Not Done' },
     },
   ];
   return axios.put(`${baseURL}form/${formId}/submissions?apiKey=${JOTFORM_API_KEY}`, requestBody);
 };
 
 export const changeTodoState = (submissionID, done) => {
-  const requestBody = `submission[2]=${done}`;
+  const requestBody = `submission[2]=${done ? 'Done' : 'Not Done'}`;
   return axios.post(`${baseURL}submission/${submissionID}?apiKey=${JOTFORM_API_KEY}`, requestBody);
 };
 
@@ -38,6 +41,6 @@ export const getTodos = formId => axios.get(`${baseURL}form/${formId}/submission
   .then(response => response.data.content)
   .then(submissions => submissions.map(submission => ({
     id: submission.id,
-    name: submission.answers['1'].prettyFormat,
-    done: submission.answers['2'].prettyFormat === 'true',
+    name: submission.answers['1'].answer,
+    done: submission.answers['2'].answer === 'Done',
   })));
